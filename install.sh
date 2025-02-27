1ad2e4c55cbd31daecbd99d7d08d0fb743d59593
AM | zxr <87238535+mub-drk@users.noreply.github.com>
2025-02-27 19:54:34 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #install zsh with oh-my-zsh
-sudo pacman -Syu zsh curl fastfetch firefox --noconfirm
+sudo pacman -Syu xorg xorg-xinit zsh curl fastfetch firefox ly --noconfirm
+sudo systemctl enable ly.service
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #download neded pkg(s)
 sudo pacman -S btop git python-pywal --noconfirm
 mkdir ~/yay/ && cd ~/yay/ && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
-yay -S rofi-git zen-browser-bin polybar-git alacritty-git yazi --noconfirm
+yay -S rofi-git zen-browser-bin polybar-git alacritty-git yazi feh python-pywalfox --noconfirm
 
 #add my config
+git clone https://github.com/mub-drk/i3-dots.git
+cd i3-dots
+cp -r alacritty i3 polybar yazi ~/.config/
+cp -r walls ~/rice/
 
+printf "\e[32m%s\e[0m\n" "install the pywalfox extention in firefox and reboot now"
