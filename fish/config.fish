be9cbe0b7ab83ff737c158b4e2b6999b464770b8
mub-drk <87238535+mub-drk@users.noreply.github.com>
2026-02-27 19:23:27 +0600

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,25 @@
+function fish_prompt -d "Write out the prompt"
+    # This shows up as USER@HOST /home/user/ >, with the directory colored
+    # $USER and $hostname are set by fish, so you can just use them
+    # instead of using `whoami` and `hostname`
+    printf '%s@%s %s%s%s > ' $USER $hostname \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+end
+
+if status is-interactive # Commands to run in interactive sessions can go here
+
+    # No greeting
+    set fish_greeting
+
+    # Use starship
+    starship init fish | source
+
+    # Aliases
+    alias clear "printf '\033[2J\033[3J\033[1;1H'" # fix: kitty doesn't clear properly
+    alias celar "printf '\033[2J\033[3J\033[1;1H'"
+    alias claer "printf '\033[2J\033[3J\033[1;1H'"
+    alias ls 'eza --icons'
+    alias pamcan pm
+    alias q 'qs -c ii'
+
+end
